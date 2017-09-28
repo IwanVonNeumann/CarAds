599074b3539def85815d105e2e0d46358dc0a2c4
Iwan Ruskiewicz <ivans.ruskevics@ideaportriga.lv>
2017-09-28 11:43:39 +0300

    filtration by empty secondary features list introduced

--- transformation/remove_incomplete.py
+++ transformation/remove_incomplete.py
@@ -1,5 +1,7 @@
 def remove_incomplete(items, log=False):
-    return remove_by_primary_attribute_none(items, log=log)
+    items = remove_by_empty_secondary_features(items, log=log)
+    items = remove_by_primary_attribute_none(items, log=log)
+    return items
 
 
 def remove_by_primary_attribute_none(items, log=False):
@@ -21,4 +23,12 @@ def remove_by_value(items, field, value, log=False):
     if log:
         records_removed = len(items) - len(filtered)
         print("Removing by {}={}: {}".format(field, value, records_removed))
+    return filtered
+
+
+def remove_by_empty_secondary_features(items, log=False):
+    filtered = [x for x in items if len(x["secondary_features"]) > 0]
+    if log:
+        records_removed = len(items) - len(filtered)
+        print("Removing by empty secondary_features: {}".format(records_removed))
     return filtered
